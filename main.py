44a24cd031f21659bf146b2962eeb59643e116d0
Ethan Padden <ethanpadden@gmail.com>
2023-06-12 12:20:58 +0100

    Add fitness function and evaluate population

--- main.py
+++ main.py
@@ -2,10 +2,48 @@ import numpy as np
 import pandas as pd
 from plotly import express as px
 
-def print_population(population):
-    for solution in population:
+def print_population(population, solution_fitnesses=None):
+   for i in range(0, len(population)):
+        solution = population[i]
         bitstring = ''.join(map(str, solution))
-        print(bitstring)
+        if solution_fitnesses != None:
+            fitness = solution_fitnesses[i]
+            print(f'{bitstring}\t{fitness}')
+        else:
+            print(bitstring)
+
+
+def fitness(solution):
+    '''What could be a fitness function here?
+    Maximise range
+    Minimise cost
+    Could be sum(range) - sum(cost)
+    The issue with this is if we are measuring in km and euro, the cost values far exceeds the ranges
+    this leads to negative fitness values
+    Also - the fitness value for no tower placements should intuitively be 0, and should be the worst possible outcome
+    Any other combination should lead to a fitness value more than 0
+    multiply the range by 100 - same as converting km to m
+    TODO: introduce some kind of weights - it would depend on how important each variable is
+    TODO: also - is there a maximum cost that we cannot exceed?
+    TODO: similarly - is there a minimum range below which the solution is not viable?
+    '''
+    # Get the corresponding tower placements as in the bitstring
+    tower_placements = []
+    for i in range(0, len(solution)):
+        if solution[i] == 1:
+            tower_placements.append(possible_tower_placements[i])
+        elif solution[i] != 0 and solution[i] != 1:
+            raise ValueError
+
+    # Sum the variables in this solution
+    sum_range = 0
+    sum_cost = 0
+    for tower_placement in tower_placements:
+        sum_range += tower_placement['range']
+        sum_cost += tower_placement['cost']
+
+    return (100*sum_range) - sum_cost
+
 
 if __name__ == '__main__':
     '''Single-objective problem:
@@ -34,6 +72,16 @@ if __name__ == '__main__':
     # GLOBAL VARIABLES (CAN BE ADJUSTED)
     T = 8
     population_size = 10
+    possible_tower_placements = [
+        {'range': 319, 'cost': 578},
+        {'range': 449, 'cost': 4937},
+        {'range': 771, 'cost': 4552},
+        {'range': 887, 'cost': 1819},
+        {'range': 722, 'cost': 1932},
+        {'range': 569, 'cost': 1958},
+        {'range': 760, 'cost': 3122},
+        {'range': 600, 'cost': 1320}
+    ]
 
     # INITIALISATION
     population = []
@@ -43,6 +91,17 @@ if __name__ == '__main__':
         population.append(solution)
 
     print_population(population)
+
+    # EVALUATION
+    solution_fitnesses = []
+    for solution in population:
+        solution_fitnesses.append(
+            fitness(solution)
+        )
+
+    print_population(population, solution_fitnesses)
+
+
 
 
 
