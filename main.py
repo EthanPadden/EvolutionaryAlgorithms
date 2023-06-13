50134f26434f5382223f309060f5e27d528b7d0f
Ethan Padden <ethanpadden@gmail.com>
2023-06-13 09:57:57 +0100

    Refactor code to tools file

--- main.py
+++ main.py
@@ -1,66 +1,7 @@
 from math import ceil
 import random
 import numpy as np
-import pandas as pd
-from plotly import express as px
-
-def print_population(population):
-   for solution in population:
-       bitstring = ''.join(map(str, solution['config']))
-       fitness = solution['fitness']
-       print(f'{bitstring}\t{fitness}')
-
-
-def fitness(config):
-    '''What could be a fitness function here?
-    Maximise range
-    Minimise cost
-    Could be sum(range) - sum(cost)
-    The issue with this is if we are measuring in km and euro, the cost values far exceeds the ranges
-    this leads to negative fitness values
-    Also - the fitness value for no tower placements should intuitively be 0, and should be the worst possible outcome
-    Any other combination should lead to a fitness value more than 0
-    multiply the range by 100 - same as converting km to m
-    TODO: introduce some kind of weights - it would depend on how important each variable is
-    TODO: also - is there a maximum cost that we cannot exceed?
-    TODO: similarly - is there a minimum range below which the solution is not viable?
-    '''
-    # Get the corresponding tower placements as in the bitstring
-    tower_placements = []
-    for i in range(0, len(config)):
-        if config[i] == 1:
-            tower_placements.append(possible_tower_placements[i])
-        elif config[i] != 0 and config[i] != 1:
-            raise ValueError
-
-    # Sum the variables in this solution
-    sum_range = 0
-    sum_cost = 0
-    for tower_placement in tower_placements:
-        sum_range += tower_placement['range']
-        sum_cost += tower_placement['cost']
-
-    return (100*sum_range) - sum_cost
-
-def calculate_avg_fitness():
-    sum_fitnesses = 0
-    for solution in current_generation:
-        sum_fitnesses += solution['fitness']
-
-    return sum_fitnesses/len(current_generation)
-
-def crossover(parent_a, parent_b):
-    config_a = parent_a['config']
-    config_b = parent_b['config']
-    crossover_point = ceil(len(config_a)/2)
-
-    config_c = np.concatenate((config_a[:crossover_point], config_b[crossover_point:]))
-    config_d = np.concatenate((config_b[:crossover_point], config_a[crossover_point:]))
-
-    offspring_c = {'config': config_c, 'fitness': 0}
-    offspring_d = {'config': config_d, 'fitness': 0}
-
-    return offspring_c, offspring_d
+import tools
 
 if __name__ == '__main__':
     '''Single-objective problem:
@@ -124,16 +65,12 @@ if __name__ == '__main__':
         }
         current_generation.append(solution)
 
-    print_population(current_generation)
-
     # GENERATIONAL LOOP
     while(terminate == False):
         # EVALUATION        ===================================
         for solution in current_generation:
             config = solution['config']
-            solution['fitness'] = fitness(config)
-
-        print_population(current_generation)
+            solution['fitness'] = tools.fitness(config, possible_tower_placements)
 
         # TERMINATION       ===================================
         # In this problem, we don't have an ideal fitness if we can improve the cost-range tradeoff
@@ -147,7 +84,7 @@ if __name__ == '__main__':
         # Performance stagnation?
         # Instead of a threshold value, we can take this as a percentage
         # ie if we don't see an increase of at least 5% for the average fitness, stop
-        current_avg_fitness = calculate_avg_fitness()
+        current_avg_fitness = tools.calculate_avg_fitness(current_generation)
         diff_avg_fitness = current_avg_fitness - previous_avg_fitness
         if(generation_number > 0):
             if ((diff_avg_fitness/previous_avg_fitness) * 100) < 5:
@@ -168,7 +105,7 @@ if __name__ == '__main__':
             # choose the top 2 in the population - and pop them off so we dont consider them anymore
             parent_a = sorted_population.pop(0)
             parent_b = sorted_population.pop(0)
-            offspring_c, offspring_d = crossover(parent_a, parent_b)
+            offspring_c, offspring_d = tools.crossover(parent_a, parent_b)
             next_generation.append(offspring_c)
             if len(next_generation) < len(current_generation):
                 next_generation.append(offspring_d)
